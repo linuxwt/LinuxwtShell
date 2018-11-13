abc6a607f9ccc07b2e561ab1a25cb8b101d6061a
linuxwt <wt439757183@126.com>
2018-11-13 10:32:58 +0800

    提交文件

---
+++ env_yumdocker.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# 更换国内163yum源
+yum -y install wget
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+wget wget http://mirrors.163.com/.help/CentOS7-Base-163.repo
+mv CentOS7-Base-163.repo /etc/yum.repos.d/CentOS-Base.repo
+yum clean all && yum makecache && yum -y update
+# 安装docker18.03与docker-compose  
+installdocker()
+{
+        yum -y install yum-utils device-mapper-persistent-data lvm2
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum-config-manager --enable docker-ce-edge
+        yum-config-manager --enable docker-ce-test
+        yum -y install docker-ce
+}
+docker version
+if [ $? -eq 127 ];then
+        echo "we can install docker-ce"
+        sleep 5
+        installdocker
+        docker version
+        if [ $? -lt 127 ];then
+                echo "the installation of docker-ce is ok."
+		rpm -qa | grep docker | xargs rpm -e --nodeps 
+                yum -y install docker-ce-18.03*
+        else
+                echo "the installation of docker-ce failed ,please reinstall"
+                exit -1
+        fi
+else
+        echo "docker have installed，pleae uninstall old version"
+        sleep 5
+        rpm -qa | grep docker | xargs rpm -e --nodeps
+        docker version
+        if [ $? -eq 127 ];then
+                echo "old docker have been uninstalled and you can install docker-ce"
+                sleep 5
+		installdocker
+		docker version
+		if [ $? -lt 127 ];then
+			echo "the installation of docker-ce is ok."
+			rpm -qa | grep docker | xargs rpm -e --nodeps
+		        yum -y install docker-ce-18.03*	
+		else
+			echo "the installation of docker-ce failed anad please reinstall."
+			exit -1
+		fi
+	else
+		echo "the old docker uninstalled conpletely and please uninstall again."
+		exit -1
+	fi
+fi
+systemctl start docker && systemctl enable docker && systemctl daemon-reload 
+docker_version=$(docker version | grep "Version" | awk '{print $2}' | head -n 2 | sed -n '2p')
+if [ $? -eq 0 ];then
+	echo "docker start successfully and the version is ${docker_version}"
+fi
+# 安装docker-compose并检查版本
+yum -y install epel-release  && yum -y install python-pip  && pip install docker-compose  && pip install --upgrade pip 
+docker-compose_version=$(docker-compose version | grep 'docker-compose' | awk '{print $3}')
+if [ $? -eq 0 ];then
+	echo "the docker-compose version is ${docker-compose_version}"
+fi
+# 配置docker加速拉取
+echo {\"registry-mirrors\":[\"https://nr630v1c.mirror.aliyuncs.com\"]} > /etc/docker/daemon.json
+
+
+# 安装常用工具
+yum -y install lrzsz && yum -y install openssh-clients && yum -y install telnet && yum -y install rsync 
