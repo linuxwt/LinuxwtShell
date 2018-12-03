42725e8dcedd9ea1df213e5d6f6604e82a4dcf51
linuxwt <31132875+linuxwt@users.noreply.github.com>
2018-12-03 13:00:18 +0800

    Create docker-monitor.sh

---
+++ docker-monitor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+>state.txt
+container_name=(nginx_soybean586 tomcat_soybean586 mysql_soybean586 mongo_soybean586 redis_soybean586)
+for container in ${container_name[@]}
+do
+docker ps -a | grep ${container} > /dev/null 2>&1
+if [ $? -eq 1 ];then
+    echo "The container ${container} is not exist." >> state.txt
+else 
+    echo "The container ${container} is exist." >> state.txt
+    prog=$(docker container top ${container} | grep -v PID | wc -l)
+    if [ ${prog} -gt 0 ];then
+        echo "The container ${container} state is normal." >> state.txt
+    else
+        echo "The container ${container} state is unnormal." >> state.txt
+    fi
+fi
+done
+content=$(cat state.txt | grep -Eh 'not|unnormal')
+content_num=$(cat state.txt | grep -Eh 'not|unnormal' | wc -l)
+if [ ${content_num} -eq 0  ];then
+    exit -1
+else 
+    echo "${content}" | mail -s "东北农林研究所服务器192.168.5.204" 439757183@qq.com
+    echo "${content}" | mail -s "东北农林研究所服务器192.168.5.204" 165055682@qq.com
+fi
