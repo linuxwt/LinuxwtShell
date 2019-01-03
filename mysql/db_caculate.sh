d5dda18c5c824950b495a26fab6e14b634a6142c
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-01-03 13:45:52 +0800

    Update db_caculate.sh

--- mysql/db_caculate.sh
+++ mysql/db_caculate.sh
@@ -6,12 +6,12 @@ container_name="$1"
 username="$2"
 password="$3"
 
-database_name=$(docker exec dbmysql mysql -uroot -pgooalgene@123 -e "show databases;" | grep -v Database)
+database_name=$(docker exec $1 mysql -u$2 -p$3 -e "show databases;" | grep -v Database)
 echo "${database_name}" > database.txt
 database_array=(${database_name})
 for database in ${database_array[@]}
 do
-database_data=$(docker exec dbmysql mysql -uroot -pgooalgene@123 -e "use information_schema;select concat(round(sum(data_length/1024/1024),2),'MB') as data from tables where table_schema='${database}';" | grep -v data)
+database_data=$(docker exec $1 mysql -u$2 -p$3 -e "use information_schema;select concat(round(sum(data_length/1024/1024),2),'MB') as data from tables where table_schema='${database}';" | grep -v data)
 echo "${database_data}" >> ./data.txt
 done
 paste -d "  " database.txt data.txt
