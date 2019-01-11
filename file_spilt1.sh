d5953f83e1b49a15ffcef38781889b353c843ed0
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-01-11 15:22:21 +0800

    Create file_spilt1.sh

---
+++ file_spilt1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 该脚本按照文件种第二列中的某几个匹配项来过滤文件
+echo -n "please enter the filename which you wangte to spilt and field1 field2 field3 of the condition ->"
+read file_name field1 field2 field3
+field_name=(${field1} ${field2} ${field3})
+for prog in ${field_name[@]}
+do
+a=$(cat ${file_name} | awk '{print $2}' | grep -n ${prog} | awk -F ':' '{print $1}')
+b=($a)
+for proc in ${b[@]}
+do
+sed -n "${proc}p" ${file_name} >> ${prog}.txt
+done
+done
