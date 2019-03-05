2bcf45dc426dab76996e4efb2d1c5724ed6bcb3b
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-03-05 15:33:39 +0800

    Update mysqlbak.sh

--- mysql/mysqlbak.sh
+++ mysql/mysqlbak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 本脚本用于容器部署的mysql备份
-CONTAINER_NAME="mysql_linuxwt"
+CONTAINER_NAME="容器名"
 DIR=`date +%Y%m%d`
 BACKDIR="/data/bak/"
 USER='username'
@@ -11,17 +11,25 @@ LOG="/data/bak/backup.log"
 TIME1=`date +%Y%m%d_%R`
 >$LOG
 
-docker exec  ${CONTAINER_NAME} mysql -u$USER -p$PASSWD -e'use dbname;flush logs';
-docker exec  ${CONTAINER_NAME} mysqldump  -u$USER -p$PASSWD  $DBNAME > $BACKDIR/mysql_dbname_${TIME1}.sql
+docker exec  ${CONTAINER_NAME} mysql -u$USER -p$PASSWD -e "use ${DBNAME} ;flush logs;"
+docker exec  ${CONTAINER_NAME} mysqldump  -u$USER -p$PASSWD  $DBNAME > $BACKDIR/mysql_${DBNAME}_${TIME1}.sql
 
 if [ $? -eq  0 ];then
     TIME2=`date +%Y%m%d_%R`
 
-    echo " ${TIME1} start to backup.   Mysql database backup Success at ${TIME2} " >> $LOG
+    echo " ${TIME1} start to backup specie ${DBNAME}.   Mysql database backup Success at ${TIME2} " >> $LOG
 else
     TIME2=`date +%Y%m%d_%R`
-    echo " ${TIME1} start to backup.   Mysql database backup Fail.Please check it. time: ${TIME2} " >>$LOG
+    echo " ${TIME1} start to backup specie ${DBNAME}.   Mysql database backup Fail.Please check it. time: ${TIME2} " >>$LOG
     exit 1;
 fi
 
-find $BACKDIR -type f -mtime +30 -exec rm -rf {} \;
+content=$(cat ${LOG} | grep -Eh 'Success')
+content_num=$(cat ${LOG} | grep -Eh 'Success' | wc -l)
+if [ ${content_num} -eq 0  ];then
+    echo "${content}" | mail -s "物种${DBNAME}备份失败"  wangteng@qq.com 
+else
+    echo "${content}" | mail -s "物种${DBNAME}备份成功"  wangteng@qq.com 
+fi
+
+find $BACKDIR -type f -mtime +15 -exec rm -rf {} \;
