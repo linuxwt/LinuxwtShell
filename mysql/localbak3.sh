d5fc5a648c1f8c79053e4c8b1ba1fcbb836b2e48
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-03-22 10:58:23 +0800

    Update localbak3.sh

--- mysql/localbak3.sh
+++ mysql/localbak3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 CONTAINER_NAME="***"
 DIR=`date +%Y%m%d`
@@ -10,6 +10,8 @@ IP="***"
 TIME1=`date +%Y%m%d_%R`
 
 >$LOG
+echo "${IP}" >> $LOG
+
 p=(***)
 for DBNAME in ${p[@]}
 do
@@ -17,19 +19,21 @@ do
     docker exec  ${CONTAINER_NAME} mysqldump  -u$USER -p$PASSWD  $DBNAME > $BACKDIR/mysql_${DBNAME}_${TIME1}.sql
     if [ $? -eq  0 ];then
         TIME2=`date +%Y%m%d_%R`
-        echo "${IP}" >> $LOG
+       # echo "${IP}" >> $LOG
         echo "name：${DBNAME}" >> $LOG
-        echo "备份是否成功：是" >> $LOG
-        echo "备份时间：${TIME1}-${TIME2}" >> $LOG
-        echo "备份文件位置：${BACKDIR}" >> $LOG
+        echo "state：successful" >> $LOG
+        echo "time：from ${TIME1} to ${TIME2}" >> $LOG
+        echo "position：${BACKDIR}" >> $LOG
+        echo "" >> $LOG
     else
         TIME2=`date +%Y%m%d_%R`
-        echo "${IP}" >> $LOG
-        echo "物种名称：${DBNAME}" >> $LOG
-        echo "备份是否成功：否" >> $LOG
-        echo "备份时间：${TIME1}-${TIME2}" >> $LOG
+#        echo "${IP}" >> $LOG
+        echo "name：${DBNAME}" >> $LOG
+        echo "state：failed" >> $LOG
+        echo "time：from ${TIME1} to ${TIME2}" >> $LOG
         errlog=$(docker logs -f mysql_core | tail -n 10)
-        echo "失败原因:${errlog}" >> ${LOG}
+        echo "failed reason:${errlog}" >> ${LOG}
+        echo "" >>LOG
     fi
 done
 
